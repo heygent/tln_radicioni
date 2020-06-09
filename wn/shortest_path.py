7b2b0fdc3aa4288f22b72ac71cf7d1026e9c3e5e
Alessandro Caputo <acpt1994@gmail.com>
2020-06-09 12:20:47 +0200

    my_wup_sim funzionante

---
+++ wn/shortest_path.py
@@ -0,0 +1,7 @@
+import nltk
+from nltk.corpus import wordnet as wn
+from nltk.corpus.reader import Synset
+import csv
+
+def my_shortest_path():
+    pass
--- wn/wu_palmer.py
+++ wn/wu_palmer.py
@@ -1,4 +1,3 @@
-from typing import List
 import nltk
 from nltk.corpus import wordnet as wn
 from nltk.corpus.reader import Synset
@@ -6,11 +5,11 @@ from nltk.corpus.reader import wup_similarity
 import csv
 
 
-def lcs(synset1: Synset, synset2: Synset) -> Synset:
+def lcs(sense_1: Synset, sense_2: Synset) -> Synset:
 
     # calcola tutti i percorsi degli iperonimi
-    hypernym_paths1 = synset1.hypernym_paths()
-    hypernym_paths2 = synset2.hypernym_paths()
+    hypernym_paths1 = sense_1.hypernym_paths()
+    hypernym_paths2 = sense_2.hypernym_paths()
 
     # creo due liste contenenti tutti gli iperonimi
     all_hypernims1 = {synset for synsets in hypernym_paths1 for synset in synsets}
@@ -20,51 +19,52 @@ def lcs(synset1: Synset, synset2: Synset) -> Synset:
     intersection = all_hypernims1.intersection(all_hypernims2)
 
     # tra gli antenati comuni prendo quello il cui percorso minimo alla radice è più lungo degli altri antenati
-    LCS: Synset = max(intersection, key=lambda x: x.min_depth())
+    if intersection:
+        LCS: Synset = max(intersection, key=lambda x: x.min_depth())
+    else:
+        LCS = None
 
     return LCS
 
 
-def wu_palmer():
+def wu_palmer(sense_1: Synset, sense_2: Synset):
 
+    LCS = lcs(sense_1, sense_2)
+
+    if not LCS:
+        return 0
+
+    LCS_depth = LCS.max_depth() + 1
+
+    s1_lcs_distance = sense_1.shortest_path_distance(LCS)
+    s2_lcs_distance = sense_2.shortest_path_distance(LCS)
+
+    s1_depth = s1_lcs_distance + LCS_depth
+    s2_depth = s2_lcs_distance + LCS_depth
+
+    return (2.0 * LCS_depth) / (s1_depth + s2_depth)
+
+
+def my_wup_similarity():
     nltk.download('wordnet')
 
     # print(wup_similarity(wn.synsets("Book")[0], wn.synsets("Football")[0]))
     with open('resources/WordSim353.csv') as csvfile:
         reader = csv.DictReader(csvfile)
-
         result = []
 
         for row in reader:
-            synset1: List[Synset] = wn.synsets(row["word_1"])
-            synset2: List[Synset] = wn.synsets(row["word_2"])
+            synsets_1 = wn.synsets(row["word_1"])
+            synsets_2 = wn.synsets(row["word_2"])
 
-            LCS = lcs(synset1[0], synset2[0])
-            LCS_depth = LCS.max_depth() + 1
+            max_similarity = max(wu_palmer(sense_1, sense_2)
+                            for sense_1 in synsets_1
+                            for sense_2 in synsets_2
+                            )
 
-            s1_lcs_distance = synset1[0].shortest_path_distance(LCS)
-            s2_lcs_distance = synset2[0].shortest_path_distance(LCS)
-
-            s1_depth = s1_lcs_distance + LCS_depth
-            s2_depth = s2_lcs_distance + LCS_depth
-
-            result.append(2.0 * LCS_depth / (s1_depth + s2_depth))
+            result.append(max_similarity)
 
         return result
 
-# def wup_similarity():
-#     nltk.download('wordnet')
-#
-#     # print(wup_similarity(wn.synsets("Book")[0], wn.synsets("Football")[0]))
-#     with open('resources/WordSim353.csv') as csvfile:
-#         reader = csv.DictReader(csvfile)
-#         result = []
-#         for row in reader:
-#
-#             temp_result = [
-#                 wu_palmer(sense_1, sense_2)
-#              ] for sense_1 in wn.synsets(row["word_1"] for sense_2 in wn.synsets(row["word_2"]
-
-
 if __name__ == "__main__":
-    wu_palmer()
+    my_wup_similarity()
